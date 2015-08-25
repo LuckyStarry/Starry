03787c0c6c8d1c7fb3b11c8a0c6413f41e4f38f7
SUN BO <starry@vip.qq.com>
2015-08-25 18:27:06 +0800

    ILogger

---
+++ Starry/Log/ILogger.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Starry.Log
+{
+    public interface ILogger
+    {
+        void Record(ILogDetail logDetail);
+    }
+
+    public interface ILogger<TLogDetail> : ILogger
+        where TLogDetail : ILogDetail
+    {
+        void Record(TLogDetail logDetail);
+    }
+}
---
+++ Starry/Log/LogLevel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Starry.Log
+{
+    public enum LogLevel
+    {
+        Unknown = 0,
+        Debug,
+        Info,
+        Warning,
+        Error
+    }
+}
