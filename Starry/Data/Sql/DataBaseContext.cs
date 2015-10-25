d28526ad6bab5e9354c67d5fee56a1cd453aafa8
Sun Bo <starry@vip.qq.com>
2015-10-25 19:00:21 +0800

    data base entity

---
+++ Starry/Data/Sql/DataBaseContext.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+
+namespace Starry.Data.Sql
+{
+    public class DataBaseContext
+    {
+        public DataBaseContext(DataBaseEntity dataBaseEntity)
+        {
+            this.DataBase = dataBaseEntity;
+        }
+
+        public DataBaseEntity DataBase { private set; get; }
+
+        protected virtual T DbHandle<TDbCommand, T>(TDbCommand dbCommand, Func<TDbCommand, T> dbHandle)
+            where TDbCommand : DbCommand
+        {
+            if (dbCommand.Connection == null)
+            {
+                dbCommand.Connection = this.DataBase.CreateDbConnection();
+            }
+            if (dbCommand.Connection.State != ConnectionState.Open)
+            {
+                dbCommand.Connection.Open();
+            }
+            try
+            {
+                return dbHandle.Invoke(dbCommand);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                if (dbCommand.Connection != null && dbCommand.Connection.State == ConnectionState.Open)
+                {
+                    dbCommand.Connection.Close();
+                }
+            }
+        }
+
+        public int ExecuteNonQuery(DbCommand dbCommand)
+        {
+            return this.DbHandle(dbCommand, cmd => cmd.ExecuteNonQuery());
+        }
+
+        public object ExecuteScalar(DbCommand dbCommand)
+        {
+            return this.DbHandle(dbCommand, cmd => cmd.ExecuteScalar());
+        }
+
+        public T ExecuteScalar<T>(DbCommand dbCommand)
+        {
+            return this.ExecuteScalar(dbCommand, obj => (T)Convert.ChangeType(obj, typeof(T)));
+        }
+
+        public T ExecuteScalar<T>(DbCommand dbCommand, Func<object, T> convert)
+        {
+            var objVal = this.ExecuteScalar(dbCommand);
+            return convert.Invoke(objVal);
+        }
+
+        public virtual DataSet ExecuteDataSet(DbCommand dbCommand)
+        {
+            return this.DbHandle(dbCommand, cmd => this.DataBase.CreateDbDataAdapter(cmd).GetData());
+        }
+
+        public virtual DataTable ExecuteDataTable(DbCommand dbCommand)
+        {
+            return this.DbHandle(dbCommand, cmd =>
+            {
+                var dataSet = this.DataBase.CreateDbDataAdapter(cmd).GetData();
+                if (dataSet != null && dataSet.Tables.Count > 0)
+                {
+                    return dataSet.Tables[0];
+                }
+                return null;
+            });
+        }
+
+        public virtual int ExecuteNonQuery(string commandText)
+        {
+            var command = this.DataBase.CreateDbCommand(commandText);
+            return this.ExecuteNonQuery(command);
+        }
+
+        public virtual object ExecuteScalar(string commandText)
+        {
+            var command = this.DataBase.CreateDbCommand(commandText);
+            return this.ExecuteScalar(command);
+        }
+
+        public virtual T ExecuteScalar<T>(string commandText)
+        {
+            return this.ExecuteScalar(commandText, obj => (T)Convert.ChangeType(obj, typeof(T)));
+        }
+
+        public virtual T ExecuteScalar<T>(string commandText, Func<object, T> convert)
+        {
+            var command = this.DataBase.CreateDbCommand(commandText);
+            return this.ExecuteScalar(command, convert);
+        }
+
+        public virtual DataSet ExecuteDataSet(string commandText)
+        {
+            var command = this.DataBase.CreateDbCommand(commandText);
+            return this.ExecuteDataSet(command);
+        }
+
+        public virtual DataTable ExecuteDataTable(string commandText)
+        {
+            var command = this.DataBase.CreateDbCommand(commandText);
+            return this.ExecuteDataTable(command);
+        }
+    }
+}
---
+++ Starry/Data/Sql/DataBaseEntity.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+
+namespace Starry.Data.Sql
+{
+    public abstract class DataBaseEntity
+    {
+        protected DataBaseEntity(string connectionName)
+        {
+            this.ConnectionName = connectionName;
+            var connectionSettings = System.Configuration.ConfigurationManager.ConnectionStrings[this.ConnectionName];
+            if (connectionSettings == null)
+            {
+                throw new ArgumentOutOfRangeException("connectionName");
+            }
+            this.ConnectionString = connectionSettings.ConnectionString;
+            this.ProviderName = connectionSettings.ProviderName;
+        }
+
+        public string ConnectionString { private set; get; }
+        public string ProviderName { private set; get; }
+        public string ConnectionName { private set; get; }
+
+        private DbProviderFactory _provider;
+        protected virtual DbProviderFactory DbProviderFactory
+        {
+            get
+            {
+                if (this._provider == null)
+                {
+                    this._provider = DbProviderFactories.GetFactory(this.ProviderName);
+                }
+                return this._provider;
+            }
+        }
+
+        public virtual DbConnection CreateDbConnection()
+        {
+            var dbConnection = this.DbProviderFactory.CreateConnection();
+            dbConnection.ConnectionString = this.ConnectionString;
+            return dbConnection;
+        }
+
+        public virtual DbCommand CreateDbCommand(string commandText)
+        {
+            var dbCommand = this.DbProviderFactory.CreateCommand();
+            dbCommand.CommandText = commandText;
+            return dbCommand;
+        }
+
+        public virtual DbCommand CreateDbCommand(string commandText, DbConnection dbConnection)
+        {
+            var dbCommand = this.CreateDbCommand(commandText);
+            dbCommand.Connection = dbConnection;
+            return dbCommand;
+        }
+
+        public virtual DbCommand CreateDbCommand(string commandText, DbConnection dbConnection, DbTransaction dbTransaction)
+        {
+            var dbCommand = this.CreateDbCommand(commandText, dbConnection);
+            dbCommand.Transaction = dbTransaction;
+            return dbCommand;
+        }
+
+        public virtual DbDataAdapter CreateDbDataAdapter(string commandText)
+        {
+            var dbCommand = this.DbProviderFactory.CreateCommand();
+            return this.CreateDbDataAdapter(dbCommand);
+        }
+
+        public virtual DbDataAdapter CreateDbDataAdapter(DbCommand selectCommand)
+        {
+            var dbAdapter = this.DbProviderFactory.CreateDataAdapter();
+            dbAdapter.SelectCommand = selectCommand;
+            return dbAdapter;
+        }
+    }
+}
