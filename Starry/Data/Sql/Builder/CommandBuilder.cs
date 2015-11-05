5000338ef8cd71e65dc137c4a05b809660fc4e58
Sun Bo <starry@vip.qq.com>
2015-11-05 23:34:57 +0800

    CommandBuilder append

---
+++ Starry/Data/Sql/Builder/CommandBuilder.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Starry.Data.Sql.Builder.Commands
+{
+    public abstract class CommandBuilder
+    {
+        public abstract string CommandText { get; }
+    }
+}
---
+++ Starry/Data/Sql/Builder/InsertCommandBuilder.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Starry.Data.Sql.Builder.Commands
+{
+    public class InsertCommandBuilder : CommandBuilder
+    {
+        public sealed override string CommandText
+        {
+            get { return "INSERT"; }
+        }
+    }
+}
